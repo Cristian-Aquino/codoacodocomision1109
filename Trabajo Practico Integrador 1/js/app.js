3b7e92e5a9cf28970b97cf7e793c329de4f5b60e
Cristian-Aquino <cristian.aquino0412@gmail.com>
2021-11-03 13:07:13 -0300

    Terminado TP integrador JavaScript

--- Trabajo Practico Integrador 1/js/app.js
+++ Trabajo Practico Integrador 1/js/app.js
@@ -1,12 +1,15 @@
-let container = document.getElementById("seccion");
+let container = document.getElementById("cuerpo");
+// prompt("Esta funcionando");
 
 const comprarTicket=()=>{
+
     container.innerHTML=`
     <div class="container">
         <div class="row">
-            <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 pt-2">
+            <div class="col col-md-10 offset-md-1 col-lg-8 offset-lg-2 pt-2 compraticket">
                 <div class="card-group">
-                    <div class="card">
+
+                    <div class="card me-1">
                         <div class="card-body border border-primary mr-1">
                         <h5 class="card-title text-center">Estudiantes</h5>
                         <p class="card-text text-center">Tienen un descuento</p>
@@ -18,11 +21,12 @@ const comprarTicket=()=>{
                         </p>
                         </div>
                     </div>
-                    <div class="card">
+
+                    <div class="card mx-1">
                         <div class="card-body border border-primary mr-1">
-                        <h5 class="card-title text-center">Estudiantes</h5>
+                        <h5 class="card-title text-center">Trainee</h5>
                         <p class="card-text text-center">Tienen un descuento</p>
-                        <p class="card-title text-center font-weight-bold">80%</p>
+                        <p class="card-title text-center font-weight-bold">50%</p>
                         <p class="card-text text-center">
                             <small class="text-muted">
                             * Presentar documentación
@@ -30,11 +34,12 @@ const comprarTicket=()=>{
                         </p>
                         </div>
                     </div>
-                    <div class="card">
-                        <div class="card-body border border-primary mr-1">
-                        <h5 class="card-title text-center">Estudiantes</h5>
+
+                    <div class="card ms-1">
+                        <div class="card-body border border-warning mr-1">
+                        <h5 class="card-title text-center">Junior</h5>
                         <p class="card-text text-center">Tienen un descuento</p>
-                        <p class="card-title text-center font-weight-bold">80%</p>
+                        <p class="card-title text-center font-weight-bold">15%</p>
                         <p class="card-text text-center">
                             <small class="text-muted">
                             * Presentar documentación
@@ -48,7 +53,7 @@ const comprarTicket=()=>{
         <div class="row">
             <div class="col text-uppercase text-center">
                 <small>Venta</small>
-                <h2> Valor de ticket </h2>
+                <h2> Valor de ticket $200 </h2>
             </div>
         </div>
         <div class="row">
@@ -57,14 +62,14 @@ const comprarTicket=()=>{
                     <div class="form-row">
                         <div class="d-flex">
                             <div class="form-row col-6 col-md-6 m-1">
-                                <input type="text" class="form-control" placeholder="Nombre"></input>
+                                <input id="nombre" type="text" class="form-control" placeholder="Nombre"></input>
                             </div>
                             <div class="form-row col-6 col-md-6 m-1">
-                                <input type="text" class="form-control" placeholder="Apellido"></input>
+                                <input id="apellido" type="text" class="form-control" placeholder="Apellido"></input>
                             </div>
                         </div>
-                        <div class="form-group col-12 col-md-12 m-1">
-                            <input type="email" class="form-control" placeholder="Correo"></input>
+                        <div class="form-row col-12 col-md-12 m-1 ">
+                            <input id="correo" type="email" class="form-control ajusteTamaño" placeholder="Correo"></input>
                         </div>
                         <div class="d-flex">
                             <div class="form-group col-6 col-md-6 m-1">
@@ -73,7 +78,8 @@ const comprarTicket=()=>{
                             </div>
                             <div class="form-group col-6 col-md-6 m-1">
                             <label for="">Categoria</label>
-                            <select class="form-select" id="inputSelect">
+                            <select class="form-select" id="categoria">
+                                <option value="base">-Seleccione una opcion-</option>
                                 <option value="estudiante">Estudiante</option>
                                 <option value="trainee">Trainne</option>
                                 <option value="junior">Junior</option>
@@ -81,14 +87,14 @@ const comprarTicket=()=>{
                             </div>
                         </div>
                     </div>
-                    <div class="form-row">
-                        <div class="alert alert-primary" role="alert">
+                    <div class="form-row m-1" id="pagar">
+                        <div class="alert alert-primary ajusteTamaño" role="alert">
                             Cantidad a pagar: $
                         </div>
                     </div>
                     <div class="form-row d-flex">
-                        <button class="btn btn-primary col-6 m-1">Borrar</button>
-                        <button class="btn btn-primary col-6 m-1">Resumen</button>
+                        <button id="btn-enviar" class="btn col-6 m-1 text-white" onclick="borrar()">Borrar</button>
+                        <button id="btn-enviar" type="button" class="btn col-6 m-1 text-white" onclick="resumen()">Resumen</button>
                     </div>
                 </form>
             </div>
@@ -101,4 +107,39 @@ const comprarTicket=()=>{
     `
 };
 
-comprarTicket();
+
+const resumen=()=>{
+    let datos = document.querySelectorAll("input");
+    let cantidad = datos[3].value;
+    var categoria = document.getElementById("categoria").value;
+    let precioFinal;
+    if(categoria =="estudiante"){
+        precioFinal = 200 * cantidad * 0.2;
+    } else if(categoria =="trainee"){
+        precioFinal = 200 * cantidad * 0.5;
+    } else if(categoria == "junior"){
+        precioFinal = 200 * cantidad * 0.85;
+    } else {
+        precioFinal = "ERROR: FALTAN DATOS";
+    }
+    // alert("Funciona " + precioFinal);
+    document.getElementById('pagar').innerHTML = `
+    <div class="alert alert-primary ajusteTamaño" role="alert" id="pagar">
+        Cantidad a pagar: $ ${precioFinal}
+    </div>
+    `;
+
+};
+
+const borrar=()=>{
+    document.getElementById('pagar').innerHTML = `
+    <div class="alert alert-primary ajusteTamaño" role="alert" id="pagar">
+        Cantidad a pagar: $
+    </div>
+    `
+    document.getElementById("categoria").value = "base";
+    document.getElementById("contidadEntradas").value = "";
+    document.getElementById("nombre").value = "";
+    document.getElementById("apellido").value = "";
+    document.getElementById("correo").value = "";
+};
