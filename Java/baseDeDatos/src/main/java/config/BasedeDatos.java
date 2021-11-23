302692c05f69ccb46f738bad7789beda25d8e92f
Cristian-Aquino <cristian.aquino0412@gmail.com>
2021-11-23 11:57:26 -0300

    Primer dia crud

--- Java/baseDeDatos/src/main/java/config/BasedeDatos.java
+++ Java/baseDeDatos/src/main/java/config/BasedeDatos.java
@@ -1,21 +1,43 @@
 package config;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class BasedeDatos {
-    public String driver = "com.mysql.jdbc.Driver";
-    public Connection getConection(){
+    public String driver = "com.mysql.cj.jdbc.Driver";
+    public Connection getConection() throws SQLException{
         Connection conexion = null;
         try{
-            conexion = DriverManager.getConnection("jdbc:mysql//localhost:3306/comision1109", "admin", "123456");
+            Class.forName(driver);
+            conexion = DriverManager.getConnection("jdbc:mysql:://localhost:3306/comision1109", "root", "");
         }catch(SQLException e){
             System.out.println(e.toString());
         
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(BasedeDatos.class.getName()).log(Level.SEVERE, null, ex);
         }
         return conexion;
     }
-    public static void main(String [] argms){
+    public static void main(String [] argms) throws SQLException{
         BasedeDatos con = new BasedeDatos();
-        con.getConection();
-        System.out.println("Hola mundo");
+        
+        Connection conexion = null;
+        conexion = con.getConection();
+        
+        PreparedStatement ps;
+        ResultSet rs;
+        
+        ps = conexion.prepareStatement("SELECT * FROM participantes");
+        rs = ps.executeQuery();
+        
+        while(rs.next()){
+            int id = rs.getInt("id");
+            String nombre = rs.getString("nombres");
+            String apellido = rs.getString("apellidos");
+            String email = rs.getString("email");
+            System.out.println("id: " + id + "Nombres: " + nombre + "Apellidos: " + apellido + "email: " + email);
+        }
+        
+        
     }
 }
